08903fda00c206a8695c1af991d47f5b8ab161ed
Luan Victor <luanvictor2108@gmail.com>
2025-02-26 01:09:26 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 function Home() {
-    const [message, setMessage] = useState('Analisando...');
+  const [message, setMessage] = useState("Analisando...");
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setMessage('Pato detectado!!! 🦆');
+      setMessage("Pato detectado!!! 🦆");
     }, 5000);
 
     return () => clearTimeout(timer);
-  }, [])
+  }, []);
 
-    return <h1>{message}</h1>
+  return <h1>{message}</h1>;
 }
 
 export default Home;
