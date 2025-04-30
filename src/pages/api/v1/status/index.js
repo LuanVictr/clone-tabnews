9c7c7ac3913353fcdd2e25cdfe888108b177fb5e
Luan Victor <luan.silva@dsgtechnology.com.br>
2025-04-29 21:57:12 -0300

    build: adicionando jsconfig.json para configurar a root do projeto

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "../../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function statusPage(request, response) {
   const resposta = await database.query("SELECT 1 + 1 as Sum;");
