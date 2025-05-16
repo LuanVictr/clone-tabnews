a092f6c1855a363355930ce948e86e1896b60c71
Luan Victor <luan.silva@dsgtechnology.com.br>
2025-05-15 21:56:04 -0300

    feat: adicionando configuracao de banco de dados

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -2,7 +2,27 @@ import database from "infra/database.js";
 
 async function statusPage(request, response) {
   const resposta = await database.query("SELECT 1 + 1 as Sum;");
-  response.status(200).json({ message: "Esta funcionando mano" });
+  const databaseName = process.env.POSTGRES_DB;
+  console.warn(databaseName);
+  const updatedAt = new Date().toISOString();
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const numberOfConnections = await database.query({
+    text: "SELECT count(*)::int from pg_stat_activity p WHERE p.datname = $1;",
+    values: [databaseName],
+  });
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: parseInt(
+          databaseMaxConnections.rows[0].max_connections,
+        ),
+        opened_connections: numberOfConnections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default statusPage;
