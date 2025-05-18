2654ac6131687f126fd2416560b176a84591d306
Luan Victor <luan.silva@dsgtechnology.com.br>
2025-05-17 22:00:05 -0300

    console: adicionando log de error para melhor visualizacao

--- infra/database.js
+++ infra/database.js
@@ -8,9 +8,16 @@ async function query(query) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
+  console.log("Credenciais do POSTRES: ", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    await client.connect();
     const response = await client.query(query);
     return response;
   } catch (error) {
